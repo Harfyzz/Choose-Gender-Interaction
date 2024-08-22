3e7e0accfb4c389e947d5ae3290b9f49e1d8c5f7
Afeez <harfyzz@gmail.com>
2024-08-22 17:18:46 +0100

    -set up

--- Choose Gender Interaction/ContentView.swift
+++ Choose Gender Interaction/ContentView.swift
@@ -10,12 +10,52 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+                HStack{
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "arrow.left")
+                            .padding(12)
+                            .foregroundStyle(Color(.white).opacity(0.6))
+                            .background(Color("object"))
+                            .clipShape(RoundedRectangle(cornerRadius: 24 ))
+                    })
+                    Spacer()
+                    
+                }
+            VStack(spacing:16){
+                HStack{
+                    Text("Select Gender")
+                        .fontWeight(.medium)
+                    Spacer()
+                    Text("3 of 6")
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 4)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 32)
+                                .stroke()
+                                .foregroundColor(Color("secondary bg"))
+                        }
+                    
+                }
+                .foregroundColor(.white.opacity(0.6))
+                ProgressView(value: 0.33)
+                    .progressViewStyle(.linear)
+                    .tint(Color("orange"))
+            }
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("Continue")
+                        .padding()
+                        .fontWeight(.medium)
+                        .foregroundStyle(Color("object"))
+                        .frame(width: 313)
+                .background(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 64))
+            })
+        }.preferredColorScheme(.dark)
+            .padding(.horizontal, 16)
     }
 }
 
