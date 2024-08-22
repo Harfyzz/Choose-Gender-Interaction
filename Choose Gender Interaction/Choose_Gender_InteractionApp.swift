e857cabf35449e6dd51406d98f0f17fa3d36ab6c
Afeez <harfyzz@gmail.com>
2024-08-22 16:37:43 +0100

    Initial Commit

---
+++ Choose Gender Interaction/Choose_Gender_InteractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Choose_Gender_InteractionApp.swift
+//  Choose Gender Interaction
+//
+//  Created by Afeez Yunus on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Choose_Gender_InteractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
